3b67cea48a64445fccc2d0fe2e95dcbe088d8812
Mariia <soloveychikm041996@gmail.com>
2021-11-18 21:28:13 +0300

    ci

--- homework1/task5.py
+++ homework1/task5.py
@@ -20,7 +20,7 @@ def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
     max_sum = max(nums)
     while k > 0:
         for i in range(len(nums) - k + 1):
-            if sum(nums[i : i + k]) > max_sum:
-                max_sum = sum(nums[i : i + k])
+            if sum(nums[i: i + k]) > max_sum:
+                max_sum = sum(nums[i: i + k])
         k -= 1
     return max_sum
