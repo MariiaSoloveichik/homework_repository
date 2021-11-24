860f9fc59841a136d16b2dfd94e9244ed85ec848
Mariia <soloveychikm041996@gmail.com>
2021-11-24 22:39:34 +0300

    Tests

--- tests/homework4/test_task_1.py
+++ tests/homework4/test_task_1.py
@@ -1,28 +1,50 @@
+import os
+
 import pytest
 
 from homework4.task_1 import read_magic_number
 
 
-def test_positive_case(temp_file):
+@pytest.fixture()
+def create_and_del_true_file():
+    with open(file='temp_file.txt', mode='w', encoding='utf8') as fi:
+        fi.write("1\n223344\n556677\nprya\nnikc")
+    yield
+    os.remove('temp_file.txt')
+
+@pytest.fixture()
+def create_and_del_false_file():
+    with open(file='temp_file.txt', mode='w', encoding='utf8') as fi:
+        fi.write("7\n223344\n556677\nprya\nnikc")
+    yield
+    os.remove('temp_file.txt')
+
+
+@pytest.fixture()
+def create_and_del_exception_file():
+    with open(file='temp_file.txt', mode='w', encoding='utf8') as fi:
+        fi.write("a b c\n223344\n556677\nprya\nnikc")
+    yield
+    os.remove('temp_file.txt')
+
+
+def test_positive_case(create_and_del_true_file):
     """Testing positive case"""
-    temp_file.write_text("1\n223344\n556677\nprya\nnikc", encoding='utf-8')
-    assert read_magic_number(temp_file)
+    assert read_magic_number('temp_file.txt') is True
 
 
-def test_negative_case(temp_file):
+def test_negative_case(create_and_del_false_file):
     """Testing negative case"""
-    temp_file.write_text("7\n223344\n556677\nprya\nnikc", encoding='utf-8')
-    assert not read_magic_number(temp_file)
+    assert read_magic_number('temp_file.txt') is False
 
 
-def test_value_error_case(temp_file):
+def test_value_error_case(create_and_del_exception_file):
     """Testing raise of ValueError"""
-    temp_file.write_text("a b c\n223344\n556677\nprya\nnikc", encoding='utf-8')
     with pytest.raises(ValueError):
-        read_magic_number(temp_file)
+        read_magic_number('temp_file.txt')
 
 
-def test_no_file_case():
+def test_no_file_case(create_and_del_false_file):
     """Testing raise of FileNotFoundError"""
     with pytest.raises(FileNotFoundError):
         read_magic_number("non-existing-file.txt")
