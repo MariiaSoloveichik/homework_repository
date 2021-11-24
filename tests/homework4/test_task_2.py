46b5ab2cbb814ff8438c259874bf7e0e1b149fac
Mariia <soloveychikm041996@gmail.com>
2021-11-24 23:26:02 +0300

    Task 2

---
+++ tests/homework4/test_task_2.py
@@ -0,0 +1,20 @@
+from unittest.mock import patch
+
+import pytest
+
+from homework4.task_2 import count_dots_on_i
+
+
+def test_count_dots_on_i_correct_answer():
+    assert count_dots_on_i('https://example.com/') == 59
+
+
+def test_count_dots_on_i_with_mock_correct_answer():
+    path_mock = 'homework4.task.html_by_url'
+    with patch(target=path_mock, return_value='ii'):
+        assert count_dots_on_i('https://example.com/') == 2
+
+
+def test_count_dots_on_i_wrong_url():
+    with pytest.raises(ValueError):
+        count_dots_on_i('https://pryanikc.pryanikc/')
