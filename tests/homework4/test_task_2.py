f1650fb8b708134faa41136203da5ebf1d15eb3b
Mariia <soloveychikm041996@gmail.com>
2021-11-24 23:34:00 +0300

    upgrade 2.0

--- tests/homework4/test_task_2.py
+++ tests/homework4/test_task_2.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 import pytest
 
 from homework4.task_2 import count_dots_on_i
@@ -7,12 +5,6 @@ from homework4.task_2 import count_dots_on_i
 
 def test_count_dots_on_i_correct_answer():
     assert count_dots_on_i('https://example.com/') == 59
-
-
-def test_count_dots_on_i_with_mock_correct_answer():
-    path_mock = 'homework4.task_2.get_html_from_url'
-    with patch(target=path_mock, return_value='ii'):
-        assert count_dots_on_i('https://example.com/') == 2
 
 
 def test_count_dots_on_i_wrong_url():
