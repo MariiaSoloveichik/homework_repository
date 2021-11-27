b39dad750a5f7cdb813a77d05c227fef040d2fa8
Mariia <soloveychikm041996@gmail.com>
2021-11-27 17:36:36 +0300

    Task 4 done

---
+++ tests/homework4/test_task_4.py
@@ -0,0 +1,13 @@
+from homework4.task_4 import fizzbuzz
+
+
+def test_fizzbuzz_return_correct_answer():
+    assert fizzbuzz(5) == ['1', '2', 'Fizz', '4', 'Buzz']
+
+
+def test_fizzbuzz_negative_integer():
+    assert fizzbuzz(-1) == []
+
+
+def test_fizzbuzz_not_integer_element():
+    assert fizzbuzz(2.3) == ValueError
