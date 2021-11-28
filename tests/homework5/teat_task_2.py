a1e599c53dad4a11d6d6a57a24f005676f469e46
Mariia <soloveychikm041996@gmail.com>
2021-11-28 23:11:03 +0300

    Change 2.1

--- tests/homework5/teat_task_2.py
+++ tests/homework5/teat_task_2.py
@@ -2,14 +2,14 @@ from homework5.task_2 import custom_sum
 
 
 def test_doc():
-    """Testing if func doc is correct"""
+    """Testing func doc is correct"""
     custom_sum(1, 2, 3, 4)
     assert custom_sum.__doc__ == 'This function can sum any \
 objects which have __add___'
 
 
 def test_name():
-    """Testing if func name is correct"""
+    """Testing func name is correct"""
     custom_sum(1, 2, 3, 4)
     assert custom_sum.__name__ == 'custom_sum'
 
