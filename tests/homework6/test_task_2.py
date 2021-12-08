524e35b7d3132fbdccb236b8a98b4b6fe00fa04e
Mariia <soloveychikm041996@gmail.com>
2021-12-08 20:24:31 +0300

    tests

--- tests/homework6/test_task_2.py
+++ tests/homework6/test_task_2.py
@@ -15,23 +15,3 @@ def test_Teacher_attributes_2():
     opp_teacher = Teacher('Daniil', 'Shadrin')
     assert opp_teacher.first_name == 'Daniil' and \
         opp_teacher.last_name == 'Shadrin'
-
-
-def test_deadline_exception_function(teacher, student):
-    """Testing raising of DeadlineError"""
-    oop_hw = teacher.create_homework('Learn OOP', 0)
-    with pytest.raises(DeadlineError):
-        student.do_homework(oop_hw, 'I have done this hw')
-
-
-def test_homework_done_class_attribute(teacher, student):
-    """Testing that homework_done attribute is
-    the same for class and instance of class"""
-    oop_hw = teacher.create_homework('Learn OOP', 1)
-    result_1 = student.do_homework(oop_hw, 'I have done this hw')
-    teacher.check_homework(result_1)
-    temp_1 = teacher.homework_done
-
-    teacher.check_homework(result_1)
-    temp_2 = Teacher.homework_done
-    assert temp_1 == temp_2
