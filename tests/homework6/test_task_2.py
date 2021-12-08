888bd6facbf014a98b01f4ba3be79468ad83dfde
Mariia <soloveychikm041996@gmail.com>
2021-12-08 20:06:27 +0300

    name

--- tests/homework6/test_task_2.py
+++ tests/homework6/test_task_2.py
@@ -3,14 +3,14 @@ import pytest
 from homework6.task2 import DeadlineError, Student, Teacher
 
 
-def test_Student_attributes():
+def test_Student_attributes_2():
     """Testing student attributes"""
     lazy_student = Student('Roman', 'Petrov')
     assert lazy_student.first_name == 'Roman' and \
         lazy_student.last_name == 'Petrov'
 
 
-def test_Teacher_attributes():
+def test_Teacher_attributes_2():
     """Testing teacher attributes"""
     opp_teacher = Teacher('Daniil', 'Shadrin')
     assert opp_teacher.first_name == 'Daniil' and \
